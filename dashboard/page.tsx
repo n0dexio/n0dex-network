deb28380174dacaca061399aec376bda87b3cfcd
n0dexio <ezwanmaulana1@gmail.com>
2026-06-20 23:05:18 +0700

    Create page.tsx

---
+++ dashboard/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Hero />
+
+      <h2>Dashboard</h2>
+
+      <p>
+        Welcome to n0dex network.
+      </p>
+    </>
+  );
+}
