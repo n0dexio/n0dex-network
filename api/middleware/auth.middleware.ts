f5ce7e4e02ab16d991c70bc150e8fbb5236420c7
n0dex <ezwanmaulana1@gmail.com>
2026-06-22 00:21:52 +0700

    Create auth.middleware.ts

---
+++ api/middleware/auth.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const apiKey = req.headers["x-api-key"];
+
+  if (!apiKey) {
+    return res.status(401).json({
+      success: false,
+      message: "API key required"
+    });
+  }
+
+  next();
+};
